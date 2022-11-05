ec695c2684321104f1805ed67e4c9b0bb1d83322
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-11-05 19:49:23 +0100

    Do not leave a blank line at the beginning and end of the cell that writes a mock kaggle.json file

--- base/bin/preprocess_notebook.py
+++ base/bin/preprocess_notebook.py
@@ -18,15 +18,13 @@ if __name__ == "__main__":
     with open(notebook_name) as f:
         nb = nbformat.read(f, as_version=4)
 
-    write_kaggle_json_code = """
-import os
+    write_kaggle_json_code = """import os
 kaggle_home = os.path.join(os.path.expanduser("~"), ".kaggle")
 os.makedirs(kaggle_home, exist_ok=True)
 kaggle_json = os.path.join(kaggle_home, "kaggle.json")
 with open(kaggle_json, "w") as f:
     f.write('{"username": "", "key": ""}')
-os.chmod(kaggle_json, 0o600)
-"""
+os.chmod(kaggle_json, 0o600)"""
     write_kaggle_json_cell = nbformat.v4.new_code_cell(write_kaggle_json_code)
     write_kaggle_json_cell.id = "write_kaggle_json"
     nb.cells.insert(0, write_kaggle_json_cell)
