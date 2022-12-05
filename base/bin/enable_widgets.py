868790864f3fa6503a6312466265a0da6a134941
Francesco Ballarin <francesco.ballarin@unicatt.it>
2022-12-05 17:41:18 +0100

    Enable ngsolve webgui widgets

--- base/bin/enable_widgets.py
+++ base/bin/enable_widgets.py
@@ -10,3 +10,18 @@ import sys
 
 if __name__ == "__main__":
     assert len(sys.argv) == 3
+    package_name = sys.argv[1]
+    package_init_file = sys.argv[2]
+
+    assert package_name in ("itkwidgets", "pythreejs", "pyvista", "webgui_jupyter_widgets")
+    if package_name in ("itkwidgets", "pythreejs", "pyvista"):
+        pass  # Nothing to be done
+    elif package_name == "webgui_jupyter_widgets":
+        with open(package_init_file, "r") as f:
+            package_init_file_content = f.read().strip("\n")
+        package_init_file_content = package_init_file_content.replace(
+            "_IN_GOOGLE_COLAB = False", "_IN_GOOGLE_COLAB = True")
+        with open(package_init_file, "w") as f:
+            f.write(package_init_file_content)
+    else:
+        raise RuntimeError("Invalid package")
