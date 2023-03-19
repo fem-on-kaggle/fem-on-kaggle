eb6a6fc61d0708afc8747318d5571a607f7f0294
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-03-19 15:34:04 +0100

    Install cython, which is already shipped in the actual kaggle image

--- base/install_pip_packages.sh
+++ base/install_pip_packages.sh
@@ -7,6 +7,9 @@
 set -e
 set -x
 
+# Install cython, which is already shipped in the actual kaggle image
+PYTHONUSERBASE=${CONDA_PREFIX} python3 -m pip install --user cython
+
 # Install cmake (for building)
 PYTHONUSERBASE=${CONDA_PREFIX} python3 -m pip install --user cmake
 
