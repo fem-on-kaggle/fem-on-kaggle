66c637bded3a58c45f84532c20d14723bc9bfc94
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-10-14 14:46:07 +0200

    Install curl executable from conda, and use micromamba while installing

--- base/install_conda_packages.sh
+++ base/install_conda_packages.sh
@@ -7,21 +7,28 @@
 set -e
 set -x
 
-# Install ncurses from conda-forge, to fix
-# /opt/conda/lib/libtinfo.so.6: no version information available (required by bash)
-conda install -c conda-forge ncurses
+# Use micromamba instead of conda
+wget https://micro.mamba.pm/api/micromamba/linux-64/latest -O /tmp/micromamba-archive
+mkdir -p /tmp/micromamba
+tar -xvjf /tmp/micromamba-archive -C /tmp/micromamba bin/micromamba
+rm /tmp/micromamba-archive
+export PATH=/tmp/micromamba/bin:$PATH
 
 # Install libcurl, which is already shipped in the actual kaggle image
 apt remove -y -qq curl libcurl*
 apt install -y -qq git
-conda install -c conda-forge libcurl
+micromamba install -c conda-forge curl
+micromamba install -c conda-forge libcurl
 ln -s ${CONDA_PREFIX}/include/curl /usr/include/
 ln -s ${CONDA_PREFIX}/lib/libcurl* /usr/lib/
 ln -s ${CONDA_PREFIX}/lib/libssh2* /usr/lib/
 
 # Install libxml2, which is already shipped in the actual kaggle image
 apt remove -y -qq libxml2
-conda install -c conda-forge libxml2
+micromamba install -c conda-forge libxml2
 
 # Remove pkgs cache to decrease the image size
 rm -rf ${CONDA_PREFIX}/pkgs
+
+# Remove micromamba
+rm -rf /tmp/micromamba
