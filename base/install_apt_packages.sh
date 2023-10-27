ac7a8b2d7e6f349534dc2e1d7e87baa2f290ea50
Francesco Ballarin <francesco.ballarin@unicatt.it>
2023-10-27 14:46:04 +0200

    REQUIRE_LIBSTDCXX_REPLACEMENT and manual compilation of patchelf are now not needed anymore due to upstream simplifications

--- base/install_apt_packages.sh
+++ base/install_apt_packages.sh
@@ -8,16 +8,4 @@ set -e
 set -x
 
 # Install additional packages that are required to compile from source
-apt install -y -qq autoconf autoconf-archive bison build-essential cmake flex git jq libtool libtool-bin ninja-build openssh-client pkg-config rsync software-properties-common unzip wget
-
-# Install patchelf from source, as the version packaged by ubuntu may be too old
-git clone https://github.com/NixOS/patchelf.git /tmp/patchelf-src
-cd /tmp/patchelf-src
-TAGS=($(git tag -l --sort=-version:refname))
-echo "Latest tag is ${TAGS[0]}"
-git checkout ${TAGS[0]}
-./bootstrap.sh
-./configure --prefix=/usr
-make
-make install
-cd && rm -rf /tmp/patchelf-src
+apt install -y -qq autoconf autoconf-archive bison build-essential cmake flex git jq libtool libtool-bin ninja-build openssh-client patchelf pkg-config rsync software-properties-common unzip wget
